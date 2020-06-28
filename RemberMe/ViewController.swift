7ee2ede20cca3b0d3490f3a031871d3454ab0990
ThanyaniTshikonwani <thanyani.tshikonwani@umuzi.org>
2020-06-28 16:55:35 +0200

    Initial Commit

---
+++ RemberMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemberMe
+//
+//  Created by Thanyani on 2020/06/28.
+//  Copyright © 2020 Thanyani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
