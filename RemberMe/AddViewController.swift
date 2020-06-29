1cf48257430c7156db6dfb9e010e3cd7a6dac849
ThanyaniTshikonwani <thanyani.tshikonwani@umuzi.org>
2020-06-29 09:42:01 +0200

    first commit

---
+++ RemberMe/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  RemberMe
+//
+//  Created by Thanyani on 2020/06/28.
+//  Copyright © 2020 Thanyani. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
